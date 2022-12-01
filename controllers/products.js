4c584322d25c6de375698477fe9c83819dd40664
Samarth Tiwari <tiwarisamarth01@gmail.com>
2022-12-02 00:47:10 +0530

    Implemented pagination functionality

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select('name price');
+  const products = await Product.find({}).select('name price').limit(6).skip(2);
   res.status(200).json({ products, nbHits: products.length });
 };
 
@@ -32,6 +32,11 @@ const getAllProducts = async (req, res) => {
     const temp = fields.replace(',', ' ');
     result.select(temp);
   }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result.skip(skip).limit(limit);
 
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
