8a20550f346770e98c732b83c29a59d848829bc8
Samarth Tiwari <tiwarisamarth01@gmail.com>
2022-12-01 23:43:20 +0530

    improved query logic accounting for redundant query params and used regex operator on name param

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,30 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ featured: true });
+  const search = 'ab';
+  const products = await Product.find({
+    name: { $regex: search, $options: 'i' },
+  });
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
   // console.log(req.query);
-  const products = await Product.find(req.query);
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.company = { $regex: name, $options: 'i' };
+  }
+  console.log(queryObject);
+
+  const products = await Product.find(queryObject);
   res.status(200).json({ products, nbHits: products.length });
 };
 
